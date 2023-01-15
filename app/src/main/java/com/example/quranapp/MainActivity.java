73bd71a5120ba0c6ab599c014d0e671b0dd347a2
HasanSuhail47 <hasansuhail007@gmail.com>
2023-01-16 00:11:53 +0500

    Repository_Button

--- app/src/main/java/com/example/quranapp/MainActivity.java
+++ app/src/main/java/com/example/quranapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.quranapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,7 +12,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView ayat;
-    Button find;
+    Button find,search;
     EditText surah_num,ayat_num;
     Quran_data data =new Quran_data();
     Quran_info info = new Quran_info();
@@ -24,37 +25,38 @@ public class MainActivity extends AppCompatActivity {
 
         ayat = findViewById(R.id.ayat_display);
         find = findViewById(R.id.find);
+        search=findViewById(R.id.search);
         surah_num= findViewById(R.id.Surah_num);
         ayat_num= findViewById(R.id.Ayat_Num);
 
-//        find.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent i = new Intent(MainActivity.this,MainActivity2.class);
-//                String s="Hello";
-//                i.putExtra("abc",s);
-//
-//                startActivity(i);
-//            }
-//        });
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://github.com/HasanSuhail47/MS_Assignment_2_QuranApp/commits/main";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
 
         find.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                if(Integer.valueOf(surah_num.getText().toString())<114) {
-                    if (surah_num.getText().toString().equals("") && !ayat_num.getText().toString().isEmpty()) {
-                        ayat.setText((data.QuranArabicText)[Integer.valueOf(ayat_num.getText().toString())]);
-                    } else if (!surah_num.getText().toString().equals("") && ayat_num.getText().toString().equals("")) {
-                        ayat.setText((data.QuranArabicText)[info.SSP[Integer.valueOf(surah_num.getText().toString()) - 1]]);
-                    } else if (!surah_num.getText().toString().equals("") && !ayat_num.getText().toString().equals("")) {
-                        ayat.setText((data.QuranArabicText)[info.SSP[Integer.valueOf(surah_num.getText().toString()) - 1] + Integer.valueOf(ayat_num.getText().toString()) - 1]);
+                if(!surah_num.getText().toString().matches("")) {
+                    if (Integer.valueOf(surah_num.getText().toString()) < 114 && Integer.valueOf(surah_num.getText().toString()) > 0) {
+                        if (surah_num.getText().toString().equals("") && !ayat_num.getText().toString().isEmpty()) {
+                            ayat.setText((data.QuranArabicText)[Integer.valueOf(ayat_num.getText().toString())]);
+                        } else if (!surah_num.getText().toString().equals("") && ayat_num.getText().toString().equals("")) {
+                            ayat.setText((data.QuranArabicText)[info.SSP[Integer.valueOf(surah_num.getText().toString()) - 1]]);
+                        } else if (!surah_num.getText().toString().equals("") && !ayat_num.getText().toString().equals("")) {
+                            ayat.setText((data.QuranArabicText)[info.SSP[Integer.valueOf(surah_num.getText().toString()) - 1] + Integer.valueOf(ayat_num.getText().toString()) - 1]);
+                        } else {
+                            ayat.setText("not found");
+                        }
                     } else {
                         ayat.setText("not found");
                     }
-                }
-                else {
-                    ayat.setText("not found");
                 }
 
             }
